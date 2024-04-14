e2d025ef2041bade4952071ce3d52ff5255c3184
Sanjeet Kumar <oksanjeetkr124@gmail.com>
2024-04-14 12:16:18 +0530

    changes

--- day-2/conversionOperation.js
+++ day-2/conversionOperation.js
@@ -1,11 +1,11 @@
 let score = true
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN (Not A Number)
@@ -14,8 +14,28 @@ console.log(valueInNumber);
 let isLoggedIn = "Sanjeet"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
 // "Sanjeet" => true
+
+// *********** Operations **************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "hello"
+let str2 = " sanjeet"
+
+let str3 = str1 + str2;
+console.log(str3);
