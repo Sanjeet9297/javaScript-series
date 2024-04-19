d7d1d42ea28ec13f205ee2f14facf472e566a931
Sanjeet Kumar <oksanjeetkr124@gmail.com>
2024-04-19 12:44:17 +0530

    changing

---
+++ day-3/comparision.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+// Comaprision and Equallity check dono alag alag chiz hai
+
+/// ===
+
+console.log("2" === 2);
