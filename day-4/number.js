02f0acd3d646fdcd9f9438b1f1b6bb803ed7a407
Sanjeet Kumar <oksanjeetkr124@gmail.com>
2024-04-30 11:35:23 +0530

    changes

---
+++ day-4/number.js
@@ -0,0 +1,15 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const otherNum = 23.567
+
+console.log(otherNum.toPrecision(3));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString("en-IN"));
