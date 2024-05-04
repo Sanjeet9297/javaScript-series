140226dbcd88374d19472138322c52dc8d13e63d
Sanjeet Kumar <oksanjeetkr124@gmail.com>
2024-05-04 10:25:14 +0530

    add more changes

--- day-7/functions.js
+++ day-7/functions.js
@@ -39,4 +39,34 @@ function loginUSerMessage(username = "Sanju") {
 }
 
 //console.log(loginUSerMessage("Sanjeet"))
-console.log(loginUSerMessage("Sanjeet"));
+//console.log(loginUSerMessage("Sanjeet"));
+
+function calculateCartPrice(val1, val2, ...num1){   // ...REST & SPREAD OPERATOR
+  return num1
+}
+
+//console.log(calculateCartPrice(200,400,500,2000));
+
+const user = {
+  username: "Sanjeet",
+  prices: 199
+}
+
+function handleObject(anyobject){
+  console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+//handleObject(user)
+handleObject({
+  username: "Sanju",
+  price: 499
+})
+
+const myNewArray = [200, 400, 600, 800]
+
+function returnSecondValue(getArray){
+  return getArray[1]
+}
+
+//console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 600, 800]));
