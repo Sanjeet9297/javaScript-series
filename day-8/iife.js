4c210728cd6bf09381e04f28c3f9e29f932220a2
Sanjeet Kumar <oksanjeetkr124@gmail.com>
2024-05-05 11:46:29 +0530

    add more iife

---
+++ day-8/iife.js
@@ -0,0 +1,14 @@
+// Immediately Invoked function Expressions (IIFE)
+(
+function chai(){
+  // named IIFE
+console.log(`DB CONNECTED`);
+})();
+
+// Definition
+ // Execution
+
+(  (name) =>   {
+  // Simple IIFE
+  console.log(`DB CONNECTED TWO $ {name}`);
+})("Sanjeet")
