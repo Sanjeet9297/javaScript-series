3ad1d06379656048d44968c2856c5f7c5c930d5f
Sanjeet Kumar <oksanjeetkr124@gmail.com>
2024-05-06 13:22:47 +0530

    add new

---
+++ day-9/control_flow.js
@@ -0,0 +1,24 @@
+// if
+const isUserLoggedIn = true
+const temperature = 41
+
+// if( temperature === 40 ){
+//   console.log("Less than 50");
+// }
+// else{
+//   console.log("Temperature is greater than 50");
+// }
+
+// console.log("Executed");
+
+
+// < , > , <=, >=, != , ===, !==
+
+
+// const score = 200 
+
+// if(score > 100){
+//   let power = "fly"
+//   console.log(`User Power ${power}`);
+// }
+//  console.log(`User Power ${power}`);
