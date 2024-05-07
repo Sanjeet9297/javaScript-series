0ce89139b068bb7908392380897749b5e0b0965c
Sanjeet Kumar <oksanjeetkr124@gmail.com>
2024-05-07 08:02:31 +0530

    add new

---
+++ day-10/dowhile.js
@@ -0,0 +1,8 @@
+
+let score = 11
+
+do{
+  console.log(`score is ${score}`);
+  score++
+
+} while(score <= 10);
---
+++ day-10/while.js
@@ -0,0 +1,14 @@
+
+let index = 0
+// while(index <= 10){
+//   console.log(`Value of index is ${index}`);
+//   index = index + 2
+// }
+
+let myArray = ["flash", "batmat", "superhero"]
+
+let arr = 0
+while( arr < myArray.length){
+  console.log(`Value is ${myArray[arr]}`);
+  arr = arr + 1
+}
